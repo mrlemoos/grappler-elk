93e0cebc562c2962541ce4f95294c117462ff2e4
Leonardo Lemos <leonardo@sbsistemas.com.br>
2021-10-08 04:11:24 -0300

    feat(ext/logspout): adiciona arquivos de config do logspout
    O logspout é um beat responsável por loggar todos os registros de
    roteamento do Docker.  Ele transporta isso para o logstash, assim
    como qualquer outro beat, que renderiza e mantém os dados
    visíveis via Kibana.

---
+++ extensions/logspout/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# https://github.com/gliderlabs/logspout/blob/621524e/custom/build.sh
+
+set -e
+apk add --update go build-base git mercurial ca-certificates
+cd /src
+go build -ldflags "-X main.Version=$1" -o /bin/logspout
+apk del go git mercurial build-base
+rm -rf /root/go /var/cache/apk/*
+
+ln -fs /tmp/docker.sock /var/run/docker.sock
